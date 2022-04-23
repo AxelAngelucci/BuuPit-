2e5fba39ed5814fba5a56a2c4a23dbd100301158
AxelAngelucci <axeljesusangelucci@gmail.com>
2022-04-23 20:54:08 -0300

    fix cards height and add sponsors in find a job page

--- public/js/sponsors.js
+++ public/js/sponsors.js
@@ -1,4 +1,4 @@
-const sponsorsImg = [1,2,3,4,5,6,7,8];
+const sponsorsImg = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 const sponsors = document.getElementById("sponsors");
 
 for(let i = 0; i < sponsorsImg.length; i++){
